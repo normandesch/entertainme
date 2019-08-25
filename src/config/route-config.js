2978beaae6e320baad9d5bcf4328102680d7e193
Norman Desch <normandesch3@gmaill.com>
2019-08-25 19:33:21 -0400

    added talent view and controller

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const talentsRoutes = require("../routes/talents");
     app.use(staticRoutes);
+    app.use(talentsRoutes);
   }
 }
---
+++ src/routes/talents.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const talentsController = require("../controllers/talentsController");
+
+router.get("/talents", talentsController.index);
+
+module.exports = router;
