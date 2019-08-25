cf795a9699dab1e0f5ce694e90f9a824a5938985
Norman Desch <normandesch3@gmaill.com>
2019-08-25 14:56:58 -0400

    server fix

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
 
-server.listen(3000);
+const port = normalizePort(process.env.PORT) || "3000";
+app.set("port", port);
+
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNan(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
   console.log("server is listening for requests on port 3000");
