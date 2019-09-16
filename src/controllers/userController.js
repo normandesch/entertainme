0013eb207350a9e6f3a400e2a64fe7c91b57e0c3
Norman Desch <normandesch3@gmaill.com>
2019-09-16 16:30:27 -0400

    validation removal

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,58 @@
+const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+
 module.exports = {
-  signUp(req, res, next){
-    res.render("users/sign_up");
-  }
-}
+    signUp(req, res, next){
+
+        res.render("users/sign_up");
+    },
+    create(req, res, next){
+        let newUser = {
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation
+        };
+
+        userQueries.createUser(newUser, (err,user) => {
+            if (err){
+                req.flash("err", err);
+                res.redirect("/users/sign_up");
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                    req.flash("notice", "You've successfully signed in!");
+                    res.redirect("/");
+                })
+            }
+        });
+    },
+    signInForm(req, res, next){
+        res.render("users/sign_in");
+    },
+    signIn(req, res, next){
+        passport.authenticate("local")(req, res, function(){
+            if (!req.user){
+                req.flash("notice", "Sign in failed. Please try again.")
+                res.redirect("/users/sign_in");
+            } else {
+                req.flash("notice", "You've successfully signed in!");
+                res.redirect("/");
+            }
+        })
+    },
+    signOut(req, res, next){
+        req.logout();
+        req.flash("notice", "You've sucessfully signed out!");
+        res.redirect("/");
+    },
+    show(req, res, next){
+
+        userQueries.getUser(req.params.id, (err, result) => {
+            if (err || result.user === undefined){
+                req.flash("notice", "No user found with that ID.");
+                res.redirect("/");
+            } else {
+                res.render("users/show", {...result});
+            }
+        });
+    }
+};
